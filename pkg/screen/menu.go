e2b7f8b88bfb17845bda8a2feb1d442bb5fc9b54
akosgarai <szlem01@gmail.com>
2020-07-06 17:17:40 +0200

    Reflect test

--- pkg/screen/menu.go
+++ pkg/screen/menu.go
@@ -184,7 +184,7 @@ func (s *MenuScreen) Update(dt, posX, posY float64, keyStore interfaces.RoKeySto
 		if buttonStore.Get(LEFT_MOUSE_BUTTON) {
 			for i, _ := range s.options {
 				fmt.Printf("&(s.options[i].surface): %v | &(s.closestMesh): %v | &tmMesh %v\n", &(s.options[i].surface), &(s.closestMesh), &tmMesh)
-				fmt.Printf("s.options[i].surface == s.closestMesh (%v) | s.options[i].surface == tmMesh (%v) | s.closestMesh == tmMesh (%v) | reflect.DeepEqual(s.options[i].surface, s.closestMesh) (%v)\n", s.options[i].surface == s.closestMesh, s.options[i].surface == tmMesh, s.closestMesh == tmMesh, reflect.DeepEqual(s.options[i].surface, s.closestMesh))
+				fmt.Printf("s.options[i].surface == s.closestMesh (%v) | s.options[i].surface == tmMesh (%v) | s.closestMesh == tmMesh (%v) | reflect.DeepEqual(s.options[i].surface, tmMesh) (%v)\n", s.options[i].surface == s.closestMesh, s.options[i].surface == tmMesh, s.closestMesh == tmMesh, reflect.DeepEqual(s.options[i].surface, tmMesh))
 				if &(s.options[i].surface) == &(s.closestMesh) {
 					fmt.Println("Surface has been found.")
 					s.options[i].clickEvent()
