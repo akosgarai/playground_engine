76aa869edaef6717dcfc759a783b3403faa29981
akosgarai <szlem01@gmail.com>
2020-07-09 19:12:52 +0200

    Add option for generating transparent texture

--- pkg/texture/texture.go
+++ pkg/texture/texture.go
@@ -59,6 +59,15 @@ func (t *Texture) UnBind() {
 
 type Textures []*Texture
 
+// TransparentTexture creates a transparent surface with the given dimensions and returns the transparent texture.
+func (t *Textures) TransparentTexture(width, height int, uniformName string, wrapper interfaces.GLWrapper) {
+	upLeft := image.Point{0, 0}
+	bottomRight := image.Point{width, height}
+	rgba := image.NewRGBA(image.Rectangle{upLeft, bottomRight})
+
+	t.AddTextureRGBA("transparent-gen", rgba, glwrapper.CLAMP_TO_EDGE, glwrapper.CLAMP_TO_EDGE, glwrapper.LINEAR, glwrapper.LINEAR, uniformName, wrapper)
+}
+
 // AddTextureRGBA gets an RGBA and further necessary parameters, sets up a Texture, and appends it.
 func (t *Textures) AddTextureRGBA(filePath string, rgba *image.RGBA, wrapR, wrapS, minificationFilter, magnificationFilter int32, uniformName string, wrapper interfaces.GLWrapper) {
 	tex := &Texture{
