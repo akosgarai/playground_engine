6f46e96b71de7ee85d8bf58fa0de06e35db99bcc
akosgarai <szlem01@gmail.com>
2020-07-15 19:29:54 +0200

    Refactor form items

--- pkg/model/form_item_bool.go
+++ pkg/model/form_item_bool.go
@@ -14,19 +14,11 @@ import (
 const (
 	FormItemWidth  = float32(0.98)
 	FormItemLength = float32(0.1)
-	ledWidth       = float32(0.2)
-	ledHeight      = float32(0.09)
 )
 
 type FormItemBool struct {
 	*FormItemBase
-	label string
 	value bool
-}
-
-// GetLabel returns the label string of the item.
-func (fi *FormItemBool) GetLabel() string {
-	return fi.label
 }
 
 // GetValue returns the value of the form item.
@@ -40,20 +32,19 @@ func (fi *FormItemBool) SetValue(v bool) {
 }
 
 func NewFormItemBool(label string, mat *material.Material, position mgl32.Vec3, wrapper interfaces.GLWrapper) *FormItemBool {
-	m := NewFormItemBase(1.96, "Half", mat, wrapper)
+	m := NewFormItemBase(1.96, "Half", label, mat, wrapper)
 	m.GetSurface().SetPosition(position)
 	var ledTexture texture.Textures
 	ledTexture.AddTexture(baseDirModel()+"/assets/led-button.png", glwrapper.CLAMP_TO_EDGE, glwrapper.CLAMP_TO_EDGE, glwrapper.LINEAR, glwrapper.LINEAR, "tex.diffuse", wrapper)
 	ledTexture.AddTexture(baseDirModel()+"/assets/led-button.png", glwrapper.CLAMP_TO_EDGE, glwrapper.CLAMP_TO_EDGE, glwrapper.LINEAR, glwrapper.LINEAR, "tex.specular", wrapper)
-	ledPrimitive := rectangle.NewExact(ledWidth, ledHeight)
+	ledPrimitive := rectangle.NewExact(m.GetTargetWidth(), m.GetTargetHeight())
 	v, i, _ := ledPrimitive.MeshInput()
 	ledMesh := mesh.NewTexturedMaterialMesh(v, i, ledTexture, mat, wrapper)
 	ledMesh.SetParent(m.GetSurface())
-	ledMesh.SetPosition(mgl32.Vec3{0.29, -0.01, 0.0})
+	ledMesh.SetPosition(m.GetTargetPosition())
 	m.AddMesh(ledMesh)
 	return &FormItemBool{
 		FormItemBase: m,
-		label:        label,
 		value:        false,
 	}
 }
--- pkg/model/form_item_char_base.go
+++ pkg/model/form_item_char_base.go
@@ -12,12 +12,10 @@ import (
 )
 
 const (
-	writableWidth  = float32(0.40)
-	writableHeight = float32(0.09)
-
-	cursorHeight = float32(0.07)
-	cursorWidth  = float32(0.015)
-	CursorInitX  = float32(0.155)
+	ITEM_WIDTH_FULL  = float32(1.0)
+	ITEM_WIDTH_HALF  = float32(0.5)
+	ITEM_WIDTH_LONG  = float32(2.0 / 3.0)
+	ITEM_WIDTH_SHORT = float32(1.0 / 3.0)
 )
 
 var (
@@ -37,14 +35,15 @@ type FormItemBase struct {
 	*BaseModel
 	width    float32
 	sizeEnum string
+	label    string
 }
 
 // NewFormItemBase returns a FormItemBase. Its input is the width of the screen,
-// the size enum of the item. Possible values: 'Full', 'Half', 'Long', 'Short'
-// the material of the surface and a gl wrapper.
+// the size enum of the item, (Possible values: 'Full', 'Half', 'Long', 'Short')
+// the label, the material of the surface and a gl wrapper.
 // In case of invalid input enum, it panics.
 // It creates the surface mesh.
-func NewFormItemBase(w float32, sizeEnum string, mat *material.Material, wrapper interfaces.GLWrapper) *FormItemBase {
+func NewFormItemBase(w float32, sizeEnum, label string, mat *material.Material, wrapper interfaces.GLWrapper) *FormItemBase {
 	if !validFormItemEnum(sizeEnum) {
 		panic("Invalid size enum.")
 	}
@@ -53,6 +52,7 @@ func NewFormItemBase(w float32, sizeEnum string, mat *material.Material, wrapper
 		BaseModel: m,
 		width:     w,
 		sizeEnum:  sizeEnum,
+		label:     label,
 	}
 	labelPrimitive := rectangle.NewExact(fi.GetFormItemWidth(), fi.GetFormItemHeight())
 	v, i, bo := labelPrimitive.MeshInput()
@@ -96,9 +96,57 @@ func (fi *FormItemBase) GetFormItemHeight() float32 {
 	return fi.width * fi.heightMultiplier()
 }
 
+// It returns the width of the label area. (55% of the halfwidth)
+func (fi *FormItemBase) GetLabelAreaWidth() float32 {
+	return fi.width * 0.275
+}
+
 // GetSurface returns the formItemMesh
 func (fi *FormItemBase) GetSurface() interfaces.Mesh {
 	return fi.meshes[0]
+}
+
+// GetLabel returns the label string of the item.
+func (fi *FormItemBase) GetLabel() string {
+	return fi.label
+}
+
+// GetTargetHeight returns the height size of the target mesh
+// (text area, button)
+func (fi *FormItemBase) GetTargetHeight() float32 {
+	return fi.GetFormItemHeight() * 0.9
+}
+
+// GetTargetWidth returns the width size of the target mesh
+// (text area, button)
+func (fi *FormItemBase) GetTargetWidth() float32 {
+	return fi.getWidthWithoutLabel() * 0.9
+}
+func (fi *FormItemBase) getWidthWithoutLabel() float32 {
+	return fi.GetFormItemWidth() - fi.GetLabelAreaWidth()
+}
+
+// GetTargetPosition returns the position vector of the target mesh.
+func (fi *FormItemBase) GetTargetPosition() mgl32.Vec3 {
+	pX := -fi.GetFormItemWidth()/2 + fi.GetLabelAreaWidth() + (fi.getWidthWithoutLabel() / 2)
+	return mgl32.Vec3{pX, -0.01, 0.0}
+}
+
+// GetCursorHeight returns the height size of the cursor.
+func (fi *FormItemBase) GetCursorHeight() float32 {
+	return fi.GetFormItemHeight() * 0.7
+}
+
+// GetCursorWidth returns the width size of the cursor.
+func (fi *FormItemBase) GetCursorWidth() float32 {
+	return fi.GetFormItemHeight() * 0.15
+}
+
+// GetCursorInitialPosition returns the initial position vector of the cursor.
+func (fi *FormItemBase) GetCursorInitialPosition() mgl32.Vec3 {
+	targetPos := fi.GetTargetPosition()
+	targetWidth := fi.GetTargetWidth()
+	return mgl32.Vec3{targetPos.X() - targetWidth/2, -0.01, 0.0}
 }
 
 type FormItemCharBase struct {
@@ -106,47 +154,40 @@ type FormItemCharBase struct {
 	cursor        interfaces.Mesh
 	cursorOffsetX float32
 	charOffsets   []float32
-	label         string
 	value         string
 	maxLen        int
 }
 
 // NewFormItemCharBase returns a FormItemCharBase that could be the base of text based form items.
 func NewFormItemCharBase(label string, mat *material.Material, position mgl32.Vec3, wrapper interfaces.GLWrapper) *FormItemCharBase {
-	m := NewFormItemBase(1.96, "Half", mat, wrapper)
+	m := NewFormItemBase(1.96, "Half", label, mat, wrapper)
 	m.GetSurface().SetPosition(position)
 	var writableTexture texture.Textures
 	writableTexture.AddTexture(baseDirModel()+"/assets/paper.png", glwrapper.CLAMP_TO_EDGE, glwrapper.CLAMP_TO_EDGE, glwrapper.LINEAR, glwrapper.LINEAR, "tex.diffuse", wrapper)
 	writableTexture.AddTexture(baseDirModel()+"/assets/paper.png", glwrapper.CLAMP_TO_EDGE, glwrapper.CLAMP_TO_EDGE, glwrapper.LINEAR, glwrapper.LINEAR, "tex.specular", wrapper)
-	writablePrimitive := rectangle.NewExact(writableWidth, writableHeight)
+	writablePrimitive := rectangle.NewExact(m.GetTargetWidth(), m.GetTargetHeight())
 	v, i, bo := writablePrimitive.MeshInput()
 	writableMesh := mesh.NewTexturedMaterialMesh(v, i, writableTexture, mat, wrapper)
 	writableMesh.SetParent(m.GetSurface())
-	writableMesh.SetPosition(mgl32.Vec3{0.24, -0.01, 0.0})
+	writableMesh.SetPosition(m.GetTargetPosition())
 	writableMesh.SetBoundingObject(bo)
 	m.AddMesh(writableMesh)
-	cursorPrimitive := rectangle.NewExact(cursorWidth, cursorHeight)
+	cursorPrimitive := rectangle.NewExact(m.GetCursorWidth(), m.GetCursorHeight())
 	var ctex texture.Textures
 	ctex.TransparentTexture(1, 1, 255, "tex.diffuse", wrapper)
 	ctex.TransparentTexture(1, 1, 255, "tex.specular", wrapper)
 	v, i, _ = cursorPrimitive.MeshInput()
 	cursor := mesh.NewTexturedMaterialMesh(v, i, ctex, material.Greenplastic, wrapper)
-	cursor.SetPosition(mgl32.Vec3{CursorInitX, 0.0, -0.01})
+	cursor.SetPosition(m.GetCursorInitialPosition())
 	cursor.SetParent(writableMesh)
 	return &FormItemCharBase{
 		FormItemBase:  m,
-		label:         label,
 		cursor:        cursor,
 		cursorOffsetX: 0.0,
 		charOffsets:   []float32{},
 		value:         "",
 		maxLen:        9,
 	}
-}
-
-// GetLabel returns the label string of the item.
-func (fi *FormItemCharBase) GetLabel() string {
-	return fi.label
 }
 
 // GetTarget returns the input target Mesh
@@ -157,7 +198,7 @@ func (fi *FormItemCharBase) GetTarget() interfaces.Mesh {
 // AddCursor displays a cursor on the target surface.
 func (fi *FormItemCharBase) AddCursor() {
 	fi.AddMesh(fi.cursor)
-	fi.cursor.SetPosition(mgl32.Vec3{CursorInitX - fi.cursorOffsetX, 0.0, -0.01})
+	fi.cursor.SetPosition(mgl32.Vec3{fi.GetCursorInitialPosition().X() - fi.cursorOffsetX, 0.0, -0.01})
 }
 
 // DeleteCursor removes the cursor from the meshes.
@@ -173,14 +214,14 @@ func (fi *FormItemCharBase) DeleteCursor() {
 func (fi *FormItemCharBase) MoveCursorWithOffset(offsetX float32) {
 	fi.cursorOffsetX = fi.cursorOffsetX + offsetX
 	fi.charOffsets = append(fi.charOffsets, offsetX)
-	fi.cursor.SetPosition(mgl32.Vec3{CursorInitX - fi.cursorOffsetX, 0.0, -0.01})
+	fi.cursor.SetPosition(mgl32.Vec3{fi.GetCursorInitialPosition().X() - fi.cursorOffsetX, 0.0, -0.01})
 }
 
 // StepBackCursor moves the cursor back after a character deletion.
 func (fi *FormItemCharBase) StepBackCursor() {
 	offsetX := fi.charOffsets[len(fi.charOffsets)-1]
 	fi.cursorOffsetX = fi.cursorOffsetX - offsetX
-	fi.cursor.SetPosition(mgl32.Vec3{CursorInitX - fi.cursorOffsetX, 0.0, -0.01})
+	fi.cursor.SetPosition(mgl32.Vec3{fi.GetCursorInitialPosition().X() - fi.cursorOffsetX, 0.0, -0.01})
 	fi.charOffsets = fi.charOffsets[:len(fi.charOffsets)-1]
 }
 
