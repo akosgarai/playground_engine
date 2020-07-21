6a19b3f64498478349718d561cf80d2c30c4fa28
akosgarai <szlem01@gmail.com>
2020-07-21 19:07:02 +0200

    Increase max value

--- pkg/screen/form.go
+++ pkg/screen/form.go
@@ -272,7 +272,7 @@ func (f *FormScreen) Update(dt, posX, posY float64, keyStore interfaces.RoKeySto
 		direction = mgl32.Vec3{0, -1, 0}
 	}
 	newYPos := formItemCurrentY + FormItemMoveSpeed*float32(dt)*direction.Y()
-	maxYValue := (-1 + BottomFrameLength + 0.3 - f.currentY)
+	maxYValue := (-1 + BottomFrameLength + 0.5 - f.currentY)
 	fmt.Printf("current: '%f', newY: '%f', min: '%f', max: '%f', dt: '%f'\n", formItemCurrentY, newYPos, formItemMinY, maxYValue, dt)
 	if newYPos > formItemMinY && newYPos < maxYValue {
 		formItemCurrentY = newYPos
