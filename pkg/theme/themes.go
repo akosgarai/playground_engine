d396e43daadc92b4b931800b12673372e038cdd5
akosgarai <szlem01@gmail.com>
2020-07-30 18:50:05 +0200

    Add default theme setup

---
+++ pkg/theme/themes.go
@@ -0,0 +1,24 @@
+package theme
+
+import (
+	"github.com/akosgarai/playground_engine/pkg/material"
+
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+var (
+	Default = &Theme{
+		frameWidth:              float32(2.0),
+		frameLength:             float32(0.02),
+		frameTopLeftWidth:       float32(0.1),
+		detailContentBoxHeight:  float32(0.5),
+		frameMaterial:           material.Jade,
+		menuItemDefaultMaterial: material.Whiteplastic,
+		menuItemHoverMaterial:   material.Ruby,
+		menuItemSurfaceTexture:  nil,
+		headerLabelColor:        mgl32.Vec3{0, 0, 1},
+		labelColor:              mgl32.Vec3{0, 0, 1},
+		inputColor:              mgl32.Vec3{0, 0.5, 0},
+		backgroundColor:         mgl32.Vec3{0, 0, 1},
+	}
+)
