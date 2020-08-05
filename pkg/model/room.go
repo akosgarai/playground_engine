00fae89be88bb691d87daa56a9f8a2c96d7b63c8
akosgarai <szlem01@gmail.com>
2020-08-05 22:58:25 +0200

    change door orientation

--- pkg/model/room.go
+++ pkg/model/room.go
@@ -242,6 +242,7 @@ func (b *RoomBuilder) BuildTexture() *Room {
 	door.SetPosition(b.frontDoorPosition())
 	door.SetParent(floor)
 	door.SetBoundingObject(bo)
+	door.RotateY(180)
 	door.RotateX(180)
 	m.AddMesh(door)
 
