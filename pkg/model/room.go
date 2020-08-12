718d317e34ddd79506eeed46ea14b6140f04f62c
akosgarai <szlem01@gmail.com>
2020-08-12 23:27:00 +0200

    door change calc to log

--- pkg/model/room.go
+++ pkg/model/room.go
@@ -518,7 +518,7 @@ func (r *Room) animateDoor(dt float64) {
 	dx, dy, dz := r.matrixToAngles(door.RotationTransformation())
 	fmt.Printf("---------------\nDoor Rotation angles:\ndx: %f dy: %f dz: %f\n", dx, dy, dz)
 	// Expected rotation angles:
-	dx, dy, dz = r.matrixToAngles(attachPointRotationMatrix.Mul4(mgl32.HomogRotate3DY(r.doorAnimationonAngle)))
+	dx, dy, dz = r.matrixToAngles(mgl32.HomogRotate3DY(r.doorAnimationonAngle).Mul4(attachPointRotationMatrix))
 	fmt.Printf("---------------\nExpected Rotation angles:\ndx: %f dy: %f dz: %f\n", dx, dy, dz)
 
 	if r.currentAnimationTime >= doorAnimationTime {
