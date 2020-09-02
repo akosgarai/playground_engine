916d3ab193443205e4bc06855c8ee34e5602b40d
akosgarai <szlem01@gmail.com>
2020-09-02 22:57:39 +0200

    Wing position calculation vol 6

--- pkg/model/bugmodel.go
+++ pkg/model/bugmodel.go
@@ -404,7 +404,7 @@ func (b *Bug) animateWings(dt float64) {
 	// current rotation angles of the w1:
 	//w2X, w2Y, w2Z := matrixToAngles(b.meshes[5].RotationTransformation())
 	// calculate the rotation vector of the door.
-	rotatedOrigoBasedVector := mgl32.Vec3{1.0, -sinDeg, cosDeg}.Normalize()
+	rotatedOrigoBasedVector := mgl32.Vec3{1.0, cosDeg, -sinDeg}.Normalize()
 	transformedVectorW1 := mgl32.TransformNormal(rotatedOrigoBasedVector, rotationMatrix)
 	//transformedVectorW2 := mgl32.TransformNormal(rotatedOrigoBasedVector.Mul(-1), rotationMatrix)
 	b.meshes[4].SetPosition(mgl32.Vec3{b.wing1Position.X() * transformedVectorW1.X(), b.wing1Position.Y() * transformedVectorW1.Y(), b.wing1Position.Z() * transformedVectorW1.Z()})
