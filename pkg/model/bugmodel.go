294691910e4f0eadefc0a5dea3ae043b659d7212
akosgarai <szlem01@gmail.com>
2020-09-07 22:39:19 +0200

    fixes vol 10

--- pkg/model/bugmodel.go
+++ pkg/model/bugmodel.go
@@ -419,7 +419,7 @@ func (b *Bug) animateWings(dt float64) {
 	// current rotation angles of the w1:
 	//w2X, w2Y, w2Z := matrixToAngles(b.meshes[5].RotationTransformation())
 	// calculate the rotation vector of the door.
-	rotatedOrigoBasedVector := mgl32.Vec3{0.0, sinDeg, -cosDeg}
+	rotatedOrigoBasedVector := mgl32.Vec3{0.0, -sinDeg, cosDeg}
 	transformedVectorW1 := mgl32.TransformCoordinate(rotatedOrigoBasedVector, rotationMatrix)
 	//transformedVectorW2 := mgl32.TransformNormal(rotatedOrigoBasedVector.Mul(-1), rotationMatrix)
 	b.meshes[6].SetPosition(transformedVectorW1.Mul(0.5))
