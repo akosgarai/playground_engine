b1eccebe2a952587331fbeb1951ffc59f87d43ff
akosgarai <szlem01@gmail.com>
2020-09-22 21:26:50 +0200

    Function to create full size window

--- pkg/window/window.go
+++ pkg/window/window.go
@@ -31,6 +31,32 @@ func InitGlfw(windowWidth, windowHeight int, windowTitle string) *glfw.Window {
 	return window
 }
 
+// InitGlfwFullSize returns a *glfw.Window instance.
+func InitGlfwFullSize(windowTitle string) *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(fmt.Errorf("could not initialize glfw: %v", err))
+	}
+
+	glfw.WindowHint(glfw.ContextVersionMajor, glwrapper.GL_MAJOR_VERSION)
+	glfw.WindowHint(glfw.ContextVersionMinor, glwrapper.GL_MINOR_VERSION)
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	monitor := glfw.GetPrimaryMonitor()
+	_, _, waw, wah := monitor.GetWorkarea()
+
+	window, err := glfw.CreateWindow(waw, wah, windowTitle, nil, nil)
+
+	if err != nil {
+		panic(fmt.Errorf("could not create opengl renderer: %v", err))
+	}
+
+	window.MakeContextCurrent()
+
+	return window
+}
+
 // DummyKeyCallback is responsible for the keyboard event handling with log.
 // So this function does nothing but printing out the input parameters.
 func DummyKeyCallback(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
