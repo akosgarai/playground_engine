1cf0ed752ea8ca11771709d7ec52aa4dafd768ab
akosgarai <szlem01@gmail.com>
2020-09-30 13:10:07 +0200

    Move bottom frame down a little

--- pkg/screen/screenwithframe.go
+++ pkg/screen/screenwithframe.go
@@ -132,7 +132,7 @@ func (b *ScreenWithFrameBuilder) Build() *ScreenWithFrame {
 	}
 
 	// bottom frame. it supposed to be full width long.
-	frameModel.AddMesh(b.frameRectangle(b.frameWidth, b.frameLength*aspHeight, mgl32.Vec3{0.0, 0, ZFrame}))
+	frameModel.AddMesh(b.frameRectangle(b.frameWidth, b.frameLength*aspHeight, mgl32.Vec3{0.0, -0.2, ZFrame}))
 	// left, right
 	frameModel.AddMesh(b.frameRectangle(b.frameLength, (b.frameWidth-b.frameLength)*aspHeight, mgl32.Vec3{-framePositionHorizontal, 0.0, ZFrame}))
 	frameModel.AddMesh(b.frameRectangle(b.frameLength, b.frameWidth-b.frameLength*aspHeight, mgl32.Vec3{framePositionHorizontal, 0.0, ZFrame}))
