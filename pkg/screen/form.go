97d9cf11db04b9be09c658ce9c15390410dd0f04
akosgarai <szlem01@gmail.com>
2020-10-04 21:55:14 +0200

    dcb experiment vol 5

--- pkg/screen/form.go
+++ pkg/screen/form.go
@@ -227,7 +227,7 @@ func (b *FormScreenBuilder) Build() *FormScreen {
 		textContainer := b.frameRectangle(textWidth, 0.15*aspWidth*aspHeight, textContainerPosition)
 		textContainer.RotateX(-180)
 		textContainer.RotateY(180)
-		b.charset.PrintTo(b.headerLabel, -textWidth/2, -0.05*aspWidth*aspHeight, ZText, 3.0/b.windowWidth, b.wrapper, textContainer, []mgl32.Vec3{b.headerLabelColor})
+		b.charset.PrintTo(b.headerLabel, -textWidth/2, -0.05*aspWidth*aspHeight, ZText, 3.0/b.windowWidth*aspWidth*aspHeight, b.wrapper, textContainer, []mgl32.Vec3{b.headerLabelColor})
 	}
 
 	formScreen := &FormScreen{
