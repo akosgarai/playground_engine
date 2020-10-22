962806a8d41a55f8ee55bc458a748b86abc6728a
akosgarai <szlem01@gmail.com>
2020-10-22 19:13:55 +0200

    Fix rotate right from middle

--- pkg/screen/cubeform.go
+++ pkg/screen/cubeform.go
@@ -541,7 +541,7 @@ func (f *CubeFormScreen) cameraAnimation(dt float64) {
 		dY = -f.GetCamera().GetRotationStep() * float32(dt)
 		f.SumOfRotation = f.SumOfRotation + dY
 		if f.SumOfRotation < f.RotationToRightAngle {
-			dY = dY - (f.SumOfRotation + f.RotationToRightAngle)
+			dY = dY - (f.RotationToRightAngle + f.SumOfRotation)
 		}
 		break
 	case "rotate-right-from-left":
